32910633f95e90700a43b45bbad15bb97f6ed904
Atharva Patange <164712914+AtharvaPatange@users.noreply.github.com>
2025-04-14 15:12:29 +0530

    Update firebaseConfig.ts

---
+++ firebaseConfig.ts
@@ -0,0 +1,11 @@
+// firebaseConfig.ts
+const firebaseConfig = {
+  apiKey: "AIzaSyBAPaaMmYknumBIAnH37FHvzi2jB3lM1YU",
+  authDomain: "todoapp-c9ac2.firebaseapp.com",
+  projectId: "todoapp-c9ac2",
+  storageBucket: "todoapp-c9ac2.firebasestorage.app",
+  messagingSenderId: "378909307345",
+  appId: "1:378909307345:web:0b382724153a1dc91ef0f0"
+};
+
+export default firebaseConfig;
